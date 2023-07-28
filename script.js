d4d9b08f665c27508cd1040403f1b703fef59f37
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-28 19:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,29 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+let td1=document.getElementById("output-all");
+let td2=document.getElementById("output-any");
+let start=performance.now();
+// console.log(start);
+let arr=[];
+for(let i=0;i<apiUrls.length;i++){
+
+    let prom =new Promise((resolve)=>{
+        setTimeout(() => {
+        resolve(fetch(apiUrls[i]));
+        },3000)
+    })
+    arr.push(prom);
+}
+  let prom3=Promise.all(arr);
+  prom3.then((data)=>{
+	  td1.innerText=performance.now()-start;
+    // console.log(performance.now()-start);
+  })
+let prom4=Promise.any(arr);
+  prom4.then((data)=>{
+	  td2.innerText=performance.now()-start;
+    // console.log(performance.now()-start);
+  })
 
 // You can write your code here
